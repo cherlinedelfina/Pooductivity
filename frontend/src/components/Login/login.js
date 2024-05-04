d098c919404348332b2c820dc7cded681db10c19
Cherline Delfina <cher@dyn-118-138-105-238.its.monash.edu.au>
2024-05-04 12:56:07 +1000

    d

--- frontend/src/components/Login/login.js
+++ frontend/src/components/Login/login.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import './Login.css'; // Import CSS file for styling
+import './login.css'; // Import CSS file for styling
 
 function Login() {
     const [email, setEmail] = useState('');
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -12,8 +12,7 @@ import {
 } from "react-router-dom";
 import ReactDOM from "react-dom/client";
 
-import Blankpage from "./components/BlankPage/blankpage";
-import resetPassword from "./components/ResetPassword/resetPassword";
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -23,18 +22,5 @@ root.render(
   <App />
 );
 
-{
-  /*const App: React.FC = () => {
-  return(
-      <BrowserRouter>
-      <Router />
-      </BrowserRouter>
-      
-  );
-};*/
-}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
